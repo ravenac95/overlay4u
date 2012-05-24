ff5b15ea58b161e3ecfe9c736ca314eda5b6699e
Reuven V. Gonzales <reuven@tobetter.us>
2012-05-24 11:39:01 -0700

    Starting some preliminary work

---
+++ overlay4u/__init__.py
@@ -0,0 +1,2 @@
+def mount(destination, lower_dir, upper_dir):
+    """Creates a mount"""
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import sys, os
+
+VERSION = '0.0.1-dev'
+
+LONG_DESCRIPTION = open('README.rst').read()
+
+setup(name='overlay4u',
+    version=VERSION,
+    description="overlay4u",
+    long_description=LONG_DESCRIPTION,
+    keywords='overlayfs',
+    author='Reuven V. Gonzales',
+    author_email='reuven@tobetter.us',
+    url="https://github.com/ravenac95/overlay4u",
+    license='MIT',
+    platforms='Ubuntu',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],
+    entry_points={},
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Operating System :: POSIX',
+        'Topic :: Software Development :: Build Tools',
+    ],
+)
