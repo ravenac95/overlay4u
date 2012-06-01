b32af5d9d6d280fa2e7603c470cb1182844312c8
Reuven V. Gonzales <reuven@tobetter.us>
2012-06-01 14:46:52 -0700

    Refactored tests to remove duplication

--- tests/test_overlay4u.py
+++ tests/test_overlay4u.py
@@ -1,58 +1,75 @@
 import fudge
 from nose.tools import raises
+from testkit import ContextUser
 import overlay4u
+from overlay4u.overlay import OverlayFS
 from overlay4u.overlay import AlreadyMounted
 
-@fudge.patch('subwrap.run')
-def test_fake_mount(fake_run):
-    from overlay4u.overlay import OverlayFS
+class GenericOverlay4uSetup(object):
+    def base_setup(self):
+        # Patch all of these objects for all tests
+        context = fudge.patch('subwrap.run', 
+                'overlay4u.overlay.ensure_directories',
+                'overlay4u.overlay.MountTable',
+                )
+        self.context_user = ContextUser(context)
+        self.fake_run, self.fake_ensure, self.fake_mount_table = self.context_user.enter()
+
+    def expect_is_mounted(self, with_load=False):
+        mount_table = self.fake_mount_table
+        if with_load:
+            mount_table = mount_table.expects('load').returns_fake()
+        mount_table.expects('is_mounted').returns(True)
     
-    # Setup subwrap.run
-    fake_run.expects_call()
+    def expect_not_is_mounted(self, with_load=False):
+        mount_table = self.fake_mount_table
+        if with_load:
+            mount_table = mount_table.expects('load').returns_fake()
+        mount_table.expects('is_mounted').returns(False)
+
+    def base_teardown(self):
+        self.context_user.exit()
+
+class TestOverlay4u(GenericOverlay4uSetup):
+    def setup(self):
+        self.base_setup()
+        self.fake_run.expects_call()
+        self.fake_ensure.expects_call()
+
+    def teardown(self):
+        self.base_teardown()
+
+    @fudge.test
+    def test_fake_mount_using_inject(self):
+        self.expect_not_is_mounted()
+
+        overlay = overlay4u.mount('mount_point', 'lower', 'upper',
+                mount_table=self.fake_mount_table)
+
+        assert isinstance(overlay, OverlayFS) == True
+        assert overlay.mount_point == 'mount_point'
+        assert overlay.lower_dir == 'lower'
+        assert overlay.upper_dir == 'upper'
+
+    @fudge.test
+    def test_fake_mount_with_fake_table(self):
+        self.expect_not_is_mounted(with_load=True)
     
-    # Setup fake mount table
-    fake_mount_table = fudge.Fake('mount_table')
-    fake_mount_table.expects('is_mounted').returns(False)
+        overlay = overlay4u.mount('mount_point', 'lower', 'upper')
 
-    overlay = overlay4u.mount('mount_point', 'lower', 'upper',
-            mount_table=fake_mount_table)
+    @raises(AlreadyMounted)
+    @fudge.test
+    def test_fake_mount_twice(self):
+        self.expect_is_mounted(with_load=True)
 
-    assert isinstance(overlay, OverlayFS) == True
-    assert overlay.mount_point == 'mount_point'
-    assert overlay.lower_dir == 'lower'
-    assert overlay.upper_dir == 'upper'
+        overlay = overlay4u.mount('mount_point', 'lower', 'upper')
 
-@fudge.patch('subwrap.run', 'overlay4u.overlay.MountTable')
-def test_fake_mount_with_fake_table(fake_run, fake_table_class):
-    fake_run.expects_call()
-    fake_table = fake_table_class.expects('load').returns_fake()
-    fake_table.expects('is_mounted').returns(False)
+    @fudge.test
+    def test_fake_unmount(self):
+        # Tests that unmount method exists. 
+        # FIXME we need to make this a bit better
 
-    overlay = overlay4u.mount('mount_point', 'lower', 'upper')
+        self.expect_not_is_mounted(with_load=True)
 
-@raises(AlreadyMounted)
-@fudge.patch('subwrap.run')
-def test_fake_mount_twice(fake_run):
-    # Setup fake mount table checker
-    fake_mount_table = fudge.Fake('mount_table')
-    (fake_mount_table.expects('is_mounted').with_args('mount_point')
-            .returns(True))
-    
-    # Make a stub for subwrap.run. just in case it gets through
-    fake_run.expects_call()
-
-    # Use dependency injection to change mount verification technique
-    overlay = overlay4u.mount('mount_point', 'lower', 'upper', 
-            mount_table=fake_mount_table)
-
-@fudge.patch('subwrap.run', 'overlay4u.overlay.MountTable')
-def test_fake_unmount(fake_run, fake_table_class):
-    # Tests that unmount method exists. 
-    # FIXME we need to make this a bit better
-    fake_run.expects_call()
-    
-    fake_table = fake_table_class.expects('load').returns_fake()
-    fake_table.expects('is_mounted').returns(False)
-
-    overlay = overlay4u.mount('mount_point', 'lower', 'upper')
-    overlay.unmount()
+        overlay = overlay4u.mount('mount_point', 'lower', 'upper')
+        overlay.unmount()
